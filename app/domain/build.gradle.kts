30a001f6d555dcad598371ddf57afcdecabd2b22
Bartosz Kosarzycki <kosiara87@gmail.com>
2025-02-23 22:24:06 +0100

    test dependencies

--- app/domain/build.gradle.kts
+++ app/domain/build.gradle.kts
@@ -52,9 +52,8 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
-
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
-    testImplementation("io.mockk:mockk:1.13.5")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
-    testImplementation("app.cash.turbine:turbine:1.0.0")
+    testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.mockk)
+    testImplementation(libs.junit.jupiter)
+    testImplementation(libs.turbine.test)
 }
